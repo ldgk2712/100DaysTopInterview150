bab06905bc7b958e62330d3efe6035e5a71e03db
ldgk2712 <ldgk2712@gmail.com>
2024-05-14 09:07:35 +0700

    new

--- Day1/removeDupEle2.py
+++ Day1/removeDupEle2.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        k = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k-2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+# Time complexity: O(n)
+s = Solution()
+print(s.removeDuplicates([1,1,1,2,2,3])) # 5
